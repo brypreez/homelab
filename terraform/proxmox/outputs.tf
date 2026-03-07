94f8671754a33c8e9e0c88d48edd7bf5f7a1b58b
root <root@enode-a.lab>
2026-03-07 03:49:28 -0500

    Fix: Switch to bpg/proxmox provider, correct resource schema, validated plan

--- terraform/proxmox/outputs.tf
+++ terraform/proxmox/outputs.tf
@@ -1,15 +1,15 @@
 output "k8s_worker_ips" {
   description = "IP addresses of provisioned K8s worker nodes"
   value = {
-    for name, vm in proxmox_vm_qemu.k8s_worker :
-    name => var.k8s_workers[index(var.k8s_workers.*.name, name)].ip
+    for name, vm in proxmox_virtual_environment_vm.k8s_worker :
+    name => vm.initialization[0].ip_config[0].ipv4[0].address
   }
 }
 
 output "k8s_worker_vmids" {
   description = "VM IDs of provisioned K8s worker nodes"
   value = {
-    for name, vm in proxmox_vm_qemu.k8s_worker :
-    name => vm.vmid
+    for name, vm in proxmox_virtual_environment_vm.k8s_worker :
+    name => vm.vm_id
   }
 }
